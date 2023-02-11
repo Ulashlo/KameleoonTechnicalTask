899095e1a4d8e9ce763a22723b60e59f15329e81
Nikolskiy Vlad <Nikolskiy.2000@mail.ru>
2023-02-11 17:28:54 +0300

    Init

---
+++ src/main/java/com/example/kameleoontechnicaltask/KameleoonTechnicalTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.kameleoontechnicaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KameleoonTechnicalTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KameleoonTechnicalTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kameleoontechnicaltask/KameleoonTechnicalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kameleoontechnicaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KameleoonTechnicalTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
