f2883fdf9cff9d86486c345b60a3ca7e1a41035e
Nikolskiy Vlad <Nikolskiy.2000@mail.ru>
2023-02-12 01:00:42 +0300

    Fix validation check in login api

--- src/main/java/com/example/kameleoontechnicaltask/controller/UserController.java
+++ src/main/java/com/example/kameleoontechnicaltask/controller/UserController.java
@@ -6,8 +6,10 @@ import com.example.kameleoontechnicaltask.service.UserService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class UserController {
